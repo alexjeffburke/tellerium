5190cd64fcbb18b00a7eeede5646ad6b0cb17f5c
Alex J Burke <alex@alexjeffburke.com>
2019-08-25 00:42:03 +0200

    Add test cases for the error conditions in fromFile().

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,3 +1,4 @@
+const expect = require("unexpected");
 const path = require("path");
 
 const Tellerium = require("../lib/index");
@@ -5,6 +6,48 @@ const Tellerium = require("../lib/index");
 const TEST_DATA = path.join(__dirname, "..", "testdata");
 
 describe("index", () => {
+  describe("fromFile()", () => {
+    it("should not allow a missing file", () => {
+      expect(
+        () => {
+          Tellerium.fromFile();
+        },
+        "to throw",
+        "Tellerium: missing test file"
+      );
+    });
+
+    it("should not allow a nonexistent path", () => {
+      expect(
+        () => {
+          Tellerium.fromFile(path.join(TEST_DATA, "nonexistent"), "unknown");
+        },
+        "to throw",
+        "Tellerium: unable to load test file"
+      );
+    });
+
+    it("should not allow a directory path", () => {
+      expect(
+        () => {
+          Tellerium.fromFile(path.join(TEST_DATA, "directory"), "unknown");
+        },
+        "to throw",
+        "Tellerium: unable to load test file"
+      );
+    });
+
+    it("should not allow a file containing invalid JSON", () => {
+      expect(
+        () => {
+          Tellerium.fromFile(path.join(TEST_DATA, "example.txt"), "unknown");
+        },
+        "to throw",
+        "Tellerium: unable to load test file"
+      );
+    });
+  });
+
   describe("when exeucuting an example side file", function() {
     this.timeout(10000);
 
